d247f9b075fb17ed6cf234c17708e1da8c959edf
JulsSkogs <rjsollenskog@gmail.com>
2023-08-02 17:54:06 -0400

    implement Supabase API

--- script.js
+++ script.js
@@ -1,37 +1,14 @@
 "use strict";
-// Placeholder location for data:
 
-const testData = [
-    {
-        host: 'Chad',
-        round1: 'Moon Landing',
-        round2: 'Princess Diana',
-        round3: 'Chemtrails',
-        round4: 'Pizzagate',
-        rank: 1,
-    },
-    {
-        host: 'JT',
-        round1: '9/11',
-        round2: 'Covid',
-        round3: 'Rothschilds',
-        round4: 'Reptile Overlords',
-        rank: 2,
-    },
-    {
-        host: 'Strider',
-        round1: 'JFK',
-        round2: 'Climate Change',
-        round3: 'Mount Rushmore Secret Chambers',
-        round4: 'Tuck Rule Game',
-        rank: 4,
-    },
-    {
-        host: 'Chris',
-        round1: 'Area 51/Aliens',
-        round2: 'Epstein',
-        round3: 'Flat Earth',
-        round4: '4Loko Gate',
-        rank: 3,
+loadStats();
+
+async function loadStats() {
+    const res = await fetch('https://czesuseuzdcfxulrcquh.supabase.co/rest/v1/drafts', {
+    headers: {
+        apikey: config.myKey,
+        authorization: config.auth
     }
-];
+});
+const data = await res.json();
+console.log(data);
+};

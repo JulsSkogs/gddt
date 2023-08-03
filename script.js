df69332634b9ef14d81bb4682464c7b10124bb09
JulsSkogs <rjsollenskog@gmail.com>
2023-08-02 21:44:04 -0400

    start implementing DOM selectors and notes for components

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 "use strict";
 
+// Selecting DOM elements
+let chadWins = document.getElementById('#chadWins');
+console.log(chadWins);
+
+// Load Data from Supabase
 loadStats();
 
 async function loadStats() {
-    const res = await fetch('https://czesuseuzdcfxulrcquh.supabase.co/rest/v1/drafts', {
+  const res = await fetch('https://czesuseuzdcfxulrcquh.supabase.co/rest/v1/drafts', {
     headers: {
-        apikey: config.myKey,
-        authorization: config.auth
+      apikey: config.myKey,
+      authorization: config.auth
     }
 });
+
 const data = await res.json();
 console.log(data);
 };
+

b6969cb11ee4963d58d638576a916b5fcb873c52
JulsSkogs <rjsollenskog@gmail.com>
2023-08-07 18:51:35 -0400

    start extracting data from JSON

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 "use strict";
 
+// Defining variables
+
+const draftData = [];
+
 // Selecting DOM elements
 let chadWins = document.getElementById('#chadWins');
 console.log(chadWins);
@@ -15,7 +19,25 @@ async function loadStats() {
     }
 });
 
-const data = await res.json();
+const data = await res.json(); // How do we elevate the data varieble to the global scope? does it need to be done directly or otherwise?
 console.log(data);
+
+updateStats(data)
 };
 
+// Calculating Win Totals
+// Separate each draft as their own array and then extract the draft rank for each drafter
+function updateStats(dataArray) {
+  let draftData = dataArray.forEach((obj) => {
+    const draftNum = obj.draftNum;
+    const draftTop = obj.draftTop;
+    const chadPicks = obj.chadPicks;
+    const jtPicks = obj.jtPicks;
+    const striderPicks = obj.jtPicks;
+    const christPicks = obj.chrisPicks;
+    const guestPicks = obj.guestPicks;
+    const drafters = obj.drafters;
+    const ranks = obj.ranks;
+    const epiNum = obj.epiNum;
+  })
+}

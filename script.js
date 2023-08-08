da930399a686f686f0a339f2cbb46c6cdede7dec
JulsSkogs <rjsollenskog@gmail.com>
2023-08-08 17:49:29 -0400

    continue work on data manipulation

--- script.js
+++ script.js
@@ -1,52 +1,35 @@
 "use strict";
 
 // Defining variables
-let draftData
-let draftNum
-let draftTop
-let chadPicks
-let jtPicks
-let striderPicks
-let chrisPicks
-let guestPicks
-let drafters
-let ranks
-let epNum
+let draftData = []; // Declare the array in the global scope
 
 // Selecting DOM elements
-let chadWins = document.getElementById('#chadWins');
-console.log(chadWins);
+
 
 // Load Data from Supabase
-loadStats();
 
-async function loadStats() {
+async function loadData() {
   const res = await fetch('https://czesuseuzdcfxulrcquh.supabase.co/rest/v1/drafts', {
     headers: {
-      apikey: config.myKey,
+      apikey:config.myKey,
       authorization: config.auth
     }
-});
+  });
+  const jsonData = await res.json();
+  return jsonData;
+}
 
-const data = await res.json();
-console.log(data);
+async function processData() {
+  const jsonData = await loadData();
 
-updateStats(data)
-};
+  for (const obj of jsonData) {
+    draftData.push(obj);
+  }
+
+  console.log(draftData);
+}
+
+processData();
 
 // Calculating Win Totals
 // Separate each draft as their own array and then extract the draft rank for each drafter
-function updateStats(dataArray) {
-  dataArray.forEach((obj) => {
-    const draftNum = obj.draftNum;
-    const draftTop = obj.draftTop;
-    const chadPicks = obj.chadPicks;
-    const jtPicks = obj.jtPicks;
-    const striderPicks = obj.jtPicks;
-    const chrisPicks = obj.chrisPicks;
-    const guestPicks = obj.guestPicks;
-    const drafters = obj.drafters;
-    const ranks = obj.ranks;
-    const epNum = obj.epNum;
-  })
-}

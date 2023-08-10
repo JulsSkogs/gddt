36d394cc5d2d071a109bedb9baa4068f9ec1b52d
JulsSkogs <rjsollenskog@gmail.com>
2023-08-10 17:59:44 -0400

    i mean who even knows anymore

--- script.js
+++ script.js
@@ -2,11 +2,14 @@
 
 // Defining variables
 let draftData = []; // Declare the array in the global scope
+const winners = {};
 
 // Selecting DOM elements
-
+ const chadWinTotal = document.querySelector(".chadWins")
 
 // Load Data from Supabase
+
+loadData();
 
 async function loadData() {
   const res = await fetch('https://czesuseuzdcfxulrcquh.supabase.co/rest/v1/drafts', {
@@ -24,12 +27,20 @@ async function processData() {
 
   for (const obj of jsonData) {
     draftData.push(obj);
+  return draftData;
   }
-
-  console.log(draftData);
 }
-
-processData();
-
+console.log(draftData);
 // Calculating Win Totals
 // Separate each draft as their own array and then extract the draft rank for each drafter
+for (const draft of draftData) {
+
+  const winner = draft.ranks[0]
+  if (!winner.length) continue
+  if (!(winner in winners)) {
+  winners[winner] = 1
+  } else {
+  winners[winner]++
+  }
+}
+console.log(winners)
